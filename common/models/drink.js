c746a104e8105e8d8a70c445c9add83a7076f56c
evangelos <karvounis.evangelos@iqu.com>
2017-07-09 19:04:16 +0200

    Added custom endpoint average

--- common/models/drink.js
+++ common/models/drink.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Drink) {
-
+  Drink.averages = function(cb) {
+    var ds = Drink.dataSource;
+    var sql = 'SELECT AVG(Rating.rating) AS average,Drink.id AS drinkId FROM Drink ' +
+        'LEFT OUTER JOIN Rating ON Rating.drinkId = Drink.id GROUP BY Drink.id';
+    ds.connector.query(sql, function(err, products) {
+      if (err) console.error(err);
+      cb(err, products);
+    });
+  };
+  Drink.remoteMethod(
+        'averages', {
+          http: {
+            path: '/averages',
+            verb: 'get',
+          },
+          returns: {
+            arg: 'data',
+            type: 'string',
+          },
+        }
+    );
 };
